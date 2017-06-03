3f25796973594131c91de52a7e76ab87c9244022
Bichuan Guo <guobichuan@gmail.com>
2017-06-03 21:13:50 +0800

    finish interface

--- src/voronoi/voronoi.cpp
+++ src/voronoi/voronoi.cpp
@@ -1,6 +1,7 @@
 #include "libqhull_r/libqhull_r.h"
 
 #include <cstdio>
+#include <iostream>
 #include <cstdlib>
 #include <cstring>
 #include <cctype>
@@ -25,15 +26,28 @@ typedef struct {
     vector<vector<int> > regions;
 } Results;
 
-Results compute_voronoi(vector<Point3Df> input, int dim, int argc, char** argv) {
+void debug_results(Results res) {
+    for (int i = 0; i < res.vertices.size(); ++i) {
+        cout << res.vertices[i].x << " " << res.vertices[i].y << " " << res.vertices[i].z << endl;
+    }
+    for (int i = 0; i < res.regions.size(); ++i) {
+        for (int j = 0; j < res.regions[i].size(); ++j) {
+            cout << res.regions[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+Results compute_voronoi(vector<Point3Df> input, int origin_dim, int argc, char** argv) {
     Results res;
     res.regions.clear();
     res.vertices.clear();
 
-    if (dim != 2 && dim != 3)
+    if (origin_dim != 2 && origin_dim != 3)
         return res;
 
     int numpoints =input.size();
+    int dim = origin_dim;
     char flags[] = "voronoi o";
     char hidden_flags[] = " ";
     coordT *points;
@@ -73,12 +87,13 @@ Results compute_voronoi(vector<Point3Df> input, int dim, int argc, char** argv) 
     int curlong, totlong;
     qh_memfreeshort(qh, &curlong, &totlong);
 
-    rewind(fp_o);
+    fclose(fp_o);
+    fp_o = fopen("outputs.txt", "r");
     int num_vertices, num_regions, dummy;
     float x, y, z;
-    fscanf(fp_o, "%d%d%d", &num_vertices, &num_regions, &dummy);
+    fscanf(fp_o, "%d%d%d%d", &dummy, &num_vertices, &num_regions, &dummy);
     for (int i = 0; i < num_vertices; ++i) {
-        if (dim == 2) {
+        if (origin_dim == 2) {
             fscanf(fp_o, "%f%f", &x, &y);
             Point3Df p;
             p.x = x;
@@ -86,7 +101,7 @@ Results compute_voronoi(vector<Point3Df> input, int dim, int argc, char** argv) 
             p.z = 0;
             res.vertices.push_back(p);
         }
-        else if (dim == 3) {
+        else if (origin_dim == 3) {
             fscanf(fp_o, "%f%f%f", &x, &y, &z);
             Point3Df p;
             p.x = x;
@@ -95,16 +110,23 @@ Results compute_voronoi(vector<Point3Df> input, int dim, int argc, char** argv) 
             res.vertices.push_back(p);
         }
     }
-
-
-
+    for (int i = 0; i < num_regions; ++i) {
+        int num_v, id_v;
+        fscanf(fp_o, "%d", &num_v);
+        vector<int> r_vs; r_vs.clear();
+        for (int j = 0; j < num_v; ++j) {
+            fscanf(fp_o, "%d", &id_v);
+            r_vs.push_back(id_v);
+        }
+        res.regions.push_back(r_vs);
+    }
     return res;
 }
 
 int main(int argc, char *argv[]) {
   int numpoints = 4;
   int dim = 2;
-  int points[] = {2,0, -2,0, 0,1, 0,-1};
+  float points[] = {2,0, -2,0, 0,1, 0,-1};
 
   vector<Point3Df> input;
   for (int i = 0; i < numpoints; ++i) {
@@ -115,6 +137,8 @@ int main(int argc, char *argv[]) {
   }
 
   auto res = compute_voronoi(input, 2, argc, argv);
+  debug_results(res);
+
   return 0;
 } /* main */
 
